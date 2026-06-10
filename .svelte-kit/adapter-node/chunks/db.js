8557ac14765a1479a111dce85d91eeb94146c449
Rhysling <rkummer@polson.com>
2026-06-10 11:36:12 -0700

    Remove tracked .svelte-kit files

--- .svelte-kit/adapter-node/chunks/db.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-//#region src/lib/server/db.ts
-var prisma = global.prisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-//#endregion
-export { prisma as t };
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C5bzGXLp.js","_app/immutable/chunks/DmYOWrb6.js","_app/immutable/chunks/Cb7llouS.js","_app/immutable/chunks/xihTtKlq.js"];
-export const stylesheets = [];
-export const fonts = [];
